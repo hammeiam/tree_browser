b0be2cee1337a8c0f1f6cc920c48545dce06e799
David Hamme <dhamme@gmail.com>
2016-03-05 20:45:14 -0800

    more aesthetic reformatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
 module.exports = {
-
-	entry: './src/index.js',
-	resolve: {
+  entry: './src/index.js',
+  resolve: {
     extensions: ["", ".js", ".jsx"]
   },
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel',
-				exclude: /node_modules/
-			},
-			{
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
         test: /\.jsx?$/,
         loader: 'eslint'
       },
       {
-				test: /\.less$/,
-				loader: 'style!css!less'
-			}
-		]
-	}
+        test: /\.less$/,
+        loader: 'style!css!less'
+      }
+    ]
+  }
 }
